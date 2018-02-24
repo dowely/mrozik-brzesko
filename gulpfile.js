f9f92e8ea0dbe2904acf8f9439d8b9276992fb2c
Marcin <marcin.ogar@gmail.com>
2018-02-24 21:47:12 +0100

    Setting up Browser-Sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-autoprefixer = require('autoprefixer');
+autoprefixer = require('autoprefixer'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 
 gulp.task('default', function(){
@@ -16,18 +18,28 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-      .pipe(postcss([cssvars, nested, autoprefixer]))
+      .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
       .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
 
+  browserSync.init({
+    notify: false,
+    server: "./app"
+  });
+
   watch('./app/index.html', function(){
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
+});
+
+gulp.task('cssInject', ['styles'], function(){
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
