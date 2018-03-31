844ae36de8afa26da91e757e1cbb68d799430912
Marcin <marcin.ogar@gmail.com>
2018-04-01 00:47:12 +0200

    Started working on desktop nav

--- app/assets/scripts/modules/Slideshow.js
+++ app/assets/scripts/modules/Slideshow.js
@@ -7,6 +7,9 @@ class Slideshow {
 
     this.slides = $('.slideshow');
     this.tabletNav = $('.slideshow-tablet-nav');
+    this.nextBtn = $('.slideshow-desktop-nav__next');
+    this.prevBtn = $('.slideshow-desktop-nav__prev');
+    this.dots = $('.slideshow-desktop-nav__dots');
 
     this.events();
 
@@ -15,6 +18,12 @@ class Slideshow {
   events() {
     this.slides.on('click', this, this.exposeSlide);
     this.tabletNav.on('click', 'li', this, this.jumpTo);
+    this.nextBtn.on('click', this, function(event){
+      event.stopPropagation();
+      var that = event.data;
+      that.moveBy(1);
+      that.showCurrentSlide();
+    });
   }
 
   exposeSlide(event) {
@@ -29,6 +38,14 @@ class Slideshow {
 
   }
 
+  showCurrentSlide() {
+    this.slides.eq(this.slideIndex).siblings().removeClass('slideshow--current');
+    this.slides.eq(this.slideIndex).addClass('slideshow--current');
+    this.slides.eq(this.slideIndex)
+      .css({opacity: "0.3"})
+      .animate({opacity: "1"}, 1000);
+  }
+
   jumpTo(event) {
     event.stopPropagation();
     var that = event.data;
@@ -40,6 +57,14 @@ class Slideshow {
       .css({opacity: "0"})
       .animate({opacity: "1"}, 1000);
 
+  }
+
+  moveBy(vector) {
+    if(this.slideIndex < this.slides.length - 1) {
+      this.slideIndex += vector;
+    } else {
+      this.slideIndex = 0;
+    }
   }
 
 }
